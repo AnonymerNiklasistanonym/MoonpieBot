9108c08d1c0af404f9569f7f3777eb452d7f3ab1
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2022-02-28 22:28:04 +0100

    Rewrite secondsToString method for better output and add days/years

--- src/other/timePeriodToString.ts
+++ src/other/timePeriodToString.ts
@@ -1,38 +1,57 @@
+const minuteInSeconds = 60;
+const hourInSeconds = minuteInSeconds * 60;
+const dayInSeconds = hourInSeconds * 24;
+const yearInSeconds = dayInSeconds * 365;
+
 /**
  * Convert a time period in seconds to a human readable string
- *
- * @todo Update so it works for time periods that have more than 24 hours
  *
  * @param seconds length of the time period in seconds
  * @returns Human readable string representation of time period
  */
 export const secondsToString = (seconds: number) => {
-  const hoursNumber = Math.floor(((seconds % 31536000) % 86400) / 3600);
+  const yearsNumber = Math.floor(seconds / yearInSeconds);
+  const daysNumber = Math.floor((seconds % yearInSeconds) / dayInSeconds);
+  const hoursNumber = Math.floor(
+    ((seconds % yearInSeconds) % dayInSeconds) / hourInSeconds
+  );
   const minutesNumber = Math.floor(
-    (((seconds % 31536000) % 86400) % 3600) / 60
+    (((seconds % yearInSeconds) % dayInSeconds) % hourInSeconds) /
+      minuteInSeconds
   );
   const secondsNumber = Math.floor(
-    (((seconds % 31536000) % 86400) % 3600) % 60
+    (((seconds % yearInSeconds) % dayInSeconds) % hourInSeconds) %
+      minuteInSeconds
   );
-  let finalString = "";
-  let addAnd = false;
+
+  const finalStringList = [];
+  if (yearsNumber > 0) {
+    finalStringList.push(`${yearsNumber} year${yearsNumber === 1 ? "" : "s"}`);
+  }
+  if (daysNumber > 0) {
+    finalStringList.push(`${daysNumber} day${daysNumber === 1 ? "" : "s"}`);
+  }
   if (hoursNumber > 0) {
-    finalString += `${hoursNumber} hour${hoursNumber === 1 ? "" : "s"}`;
-    addAnd = true;
+    finalStringList.push(`${hoursNumber} hour${hoursNumber === 1 ? "" : "s"}`);
   }
   if (minutesNumber > 0) {
-    if (addAnd) {
-      finalString += " and ";
-    }
-    finalString += `${minutesNumber} minute${minutesNumber === 1 ? "" : "s"}`;
-    addAnd = true;
+    finalStringList.push(
+      `${minutesNumber} minute${minutesNumber === 1 ? "" : "s"}`
+    );
   }
-  if (secondsNumber > 0 || addAnd === false) {
-    if (addAnd) {
-      finalString += " and ";
-    }
-    finalString += `${secondsNumber} second${secondsNumber === 1 ? "" : "s"}`;
-    addAnd = true;
+  if (secondsNumber > 0 || finalStringList.length === 0) {
+    finalStringList.push(
+      `${secondsNumber} second${secondsNumber === 1 ? "" : "s"}`
+    );
   }
+  const finalString = finalStringList.reduce((prev, curr, currentIndex) => {
+    if (currentIndex === 0) {
+      return `${curr}`;
+    } else if (currentIndex < finalStringList.length - 1) {
+      return `${prev}, ${curr}`;
+    } else {
+      return `${prev} and ${curr}`;
+    }
+  }, "");
   return finalString;
 };
