6a021cdee12adfaa7b4fae6cadaffa93edd85f91
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2022-06-21 04:28:53 +0200

    Update logging with sections for streamcompanion and osu irc

--- src/osuirc.ts
+++ src/osuirc.ts
@@ -2,6 +2,14 @@ import irc from "irc";
 import type { Logger } from "winston";
 
 /**
+ * The logging ID of this module.
+ */
+const LOG_ID_MODULE_OSU_IRC = "osu_irc";
+
+const OSU_IRC_URL = "irc.ppy.sh";
+const OSU_IRC_PORT = 6667;
+
+/**
  * Establish a osu irc connection which can then be used to send messages.
  *
  * Check the docs at https://github.com/martynsmith/node-irc/blob/master/docs/API.rst for more information.
@@ -18,12 +26,16 @@ export const createOsuIrcConnection = (
 ) => {
   // TODO Handle authentication errors
   const creationDate = new Date().toISOString();
-  const osuIrcBotInstance = new irc.Client("irc.ppy.sh", osuIrcUsername, {
+  logger.info({
+    message: `Trying to connect to ${OSU_IRC_URL}:${OSU_IRC_PORT} as ${osuIrcUsername} (creationDate=${creationDate})`,
+    section: LOG_ID_MODULE_OSU_IRC,
+  });
+  const osuIrcBotInstance = new irc.Client(OSU_IRC_URL, osuIrcUsername, {
     channels: [
       /*"#osu"*/
     ],
     password: osuIrcPassword,
-    port: 6667,
+    port: OSU_IRC_PORT,
     autoConnect: false,
   });
   osuIrcBotInstance.addListener(
@@ -31,7 +43,7 @@ export const createOsuIrcConnection = (
     (from: string, to: string, text: string, message: string) => {
       logger.info({
         message: JSON.stringify({ creationDate, from, to, text, message }),
-        section: "osu_irc",
+        section: LOG_ID_MODULE_OSU_IRC,
         subsection: "message_listener",
       });
     }
@@ -41,7 +53,7 @@ export const createOsuIrcConnection = (
     (from: string, text: string, message: string) => {
       logger.info({
         message: JSON.stringify({ creationDate, from, text, message }),
-        section: "osu_irc",
+        section: LOG_ID_MODULE_OSU_IRC,
         subsection: "pm_listener",
       });
     }
@@ -49,14 +61,14 @@ export const createOsuIrcConnection = (
   osuIrcBotInstance.addListener("error", (message: string) => {
     logger.error({
       message: JSON.stringify({ creationDate, message }),
-      section: "osu_irc",
+      section: LOG_ID_MODULE_OSU_IRC,
       subsection: "error_listener",
     });
   });
   osuIrcBotInstance.addListener("registered", (message: string) => {
     logger.info({
       message: JSON.stringify({ creationDate, message }),
-      section: "osu_irc",
+      section: LOG_ID_MODULE_OSU_IRC,
       subsection: "registered_listener",
     });
   });
@@ -64,7 +76,7 @@ export const createOsuIrcConnection = (
   osuIrcBotInstance.addListener("selfMessage", (to: string, text: string) => {
     logger.info({
       message: JSON.stringify({ creationDate, to, text }),
-      section: "osu_irc",
+      section: LOG_ID_MODULE_OSU_IRC,
       subsection: "message_sent_listener",
     });
   });
