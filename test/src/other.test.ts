2b3f465714fe360f3846418eb8fa141e1e044334
Niklas <niklas.mikeler@gmail.com>
2022-06-25 14:03:37 +0200

    Remove unused code

--- test/src/other.test.ts
+++ test/src/other.test.ts
@@ -1,8 +1,6 @@
-import timePeriodToStringTestSuit from "./other/timePeriodToString.test";
 import asyncReplaceTestSuit from "./other/asyncReplace.test";
 import { describe } from "mocha";
 
 describe("other", () => {
-  timePeriodToStringTestSuit();
   asyncReplaceTestSuit();
 });
