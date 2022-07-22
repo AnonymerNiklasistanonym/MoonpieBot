4e34c098b86990bf29c5d5b6267c7cf2beefb994
Niklas <niklas.mikeler@gmail.com>
2022-07-22 18:31:25 +0200

    Add documentation to file operations.

--- src/other/fileOperations.ts
+++ src/other/fileOperations.ts
@@ -1,14 +1,33 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 import { promises as fs } from "fs";
 
-export const fileExists = async (path: string) =>
-  !!(await fs.stat(path).catch(() => false));
+/**
+ * Return if file/directory already exists.
+ *
+ * @param filePath Path of file/directory.
+ * @returns True if exists, False if not.
+ */
+export const fileExists = async (filePath: string) =>
+  !!(await fs.stat(filePath).catch(() => false));
 
-export const readJsonFile = async <OUTPUT>(path: string) => {
-  const content = await fs.readFile(path);
+/**
+ * Read a JSON file.
+ *
+ * @param filePath Path of file/directory.
+ * @template OUTPUT The type of the JSON file.
+ * @returns The content of the JSON file (the format is not checked!).
+ */
+export const readJsonFile = async <OUTPUT>(filePath: string) => {
+  const content = await fs.readFile(filePath);
   return JSON.parse(content.toString()) as OUTPUT;
 };
 
-export const writeJsonFile = async <INPUT>(path: string, data: INPUT) => {
-  await fs.writeFile(path, JSON.stringify(data, undefined, 4));
-};
+/**
+ * Write a JSON file.
+ *
+ * @param filePath Path of file/directory.
+ * @param data The new data of the JSON file.
+ * @template INPUT The type of the JSON file to validate the data.
+ */
+export const writeJsonFile = async <INPUT>(filePath: string, data: INPUT) =>
+  await fs.writeFile(filePath, JSON.stringify(data, undefined, 4));
